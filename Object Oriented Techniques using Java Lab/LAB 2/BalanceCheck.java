8c441d54362ef34ae88afc8bed513f2e82285809
krsatyam7 <110342305+krsatyam7@users.noreply.github.com>
2022-09-08 01:58:24 +0530

    Create BalanceCheck.java

---
+++ Object Oriented Techniques using Java Lab/LAB 2/BalanceCheck.java
@@ -0,0 +1,13 @@
+package q10850;
+
+class BalanceCheck{
+        public void checkBalance(double balance){
+                if(balance < 1000){
+                        System.out.println("Balance is low");
+                }else{
+                        System.out.println("Sufficient balance");
+                }
+        }
+
+
+}
