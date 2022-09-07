f1869ea7bc7c7dffeb0c535461e70d7af5142c19
krsatyam7 <110342305+krsatyam7@users.noreply.github.com>
2022-09-08 01:51:43 +0530

    Create PrimitiveTypes.java

---
+++ Object Oriented Techniques using Java Lab/LAB 1/PrimitiveTypes.java
@@ -0,0 +1,20 @@
+class PrimitiveTypes{
+        public static void main(String[] args){
+                byte b = 0;
+                short s = 0;
+                int i = 0;
+                long l = 0;
+                boolean bool = false;
+                double d = 0;
+                float f = 0;
+
+                System.out.println("byte default value = " + b);
+                System.out.println("short default value = " + s);
+                System.out.println("int default value = " + i);
+                System.out.println("long default value = " + l);
+                System.out.println("boolean default value = " + bool);
+                System.out.println("double default value = " + d);
+                System.out.println("float default value = " + f);
+
+        }
+}
