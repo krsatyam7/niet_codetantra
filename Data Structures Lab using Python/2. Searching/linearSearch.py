b669d401dca18a37db1956e2a2b01b447ff7b57f
Kr Satyam <110342305+krsatyam7@users.noreply.github.com>
2022-09-09 16:55:37 +0530

    Update linearSearch.py

--- Data Structures Lab using Python/2. Searching/linearSearch.py
+++ Data Structures Lab using Python/2. Searching/linearSearch.py
@@ -1,3 +1,6 @@
+
+// method 1
+
 mylist=input("Enter the list of numbers: ").split()
 A=[int(i) for i in mylist]
 length=len(A)
@@ -11,3 +14,20 @@ if loc==-1:
 	print(value,"was not found.")
 else:
 	print(value,"was found at index",str(loc-1)+'.')
+
+	
+// method 2
+
+a=list(map(int,input('Enter the list of numbers: ').split()))
+num=int(input('The number to search for: '))
+flag=0
+k=0
+for i in a:
+	if i==num:
+		flag=1
+		break
+	k+=1
+if flag==1:
+	print(f'{num} was found at index {k}.')
+else:
+	print(num,'was not found.')
