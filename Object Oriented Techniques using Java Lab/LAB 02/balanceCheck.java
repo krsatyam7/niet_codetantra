d2d7685c024277abdcbda32a36ee3e657622cbab
Kr Satyam <110342305+krsatyam7@users.noreply.github.com>
2022-09-13 19:55:25 +0530

    Update balanceCheck.java

--- Object Oriented Techniques using Java Lab/LAB 02/balanceCheck.java
+++ Object Oriented Techniques using Java Lab/LAB 02/balanceCheck.java
@@ -3,9 +3,11 @@ public class BalanceCheck
 {
 	public void checkBalance(double balance)
 	{
-		if (balance<1000)
-		System.out.println("Balance is low");
-		else
-		System.out.println("Sufficient balance");
+		if (balance<1000){
+			System.out.println("Balance is low");
+		}
+		else{
+			System.out.println("Sufficient balance");
+		}
 	}
 }
