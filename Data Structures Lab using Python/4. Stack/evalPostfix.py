a6a93caae0bc7444afcfe85480b9b8eba9459ebe
Kr Satyam <110342305+krsatyam7@users.noreply.github.com>
2022-10-05 22:43:48 +0530

    Update evalPostfix.py

--- Data Structures Lab using Python/4. Stack/evalPostfix.py
+++ Data Structures Lab using Python/4. Stack/evalPostfix.py
@@ -1,31 +1,31 @@
-class evalPostfix:
-	def __init__(self,data):
-		self.top=-1
-		self.data=data
-		self.array=[]
-	def isempty(self):
-		return Turn if self.top == -1 else False
-	def peak(self):
-		return self.array[-1]
-	def pop(self):
-		if not self.isempty():
-			self.top-=1
-			return self.array.pop()
-		else:
-			return "$"
-	def push(self,op):
-		self.top+=1
-		self.array.append(op)
-	def EPF(self,exp):
-		for i in exp:
-			if i.isdigit():
-				self.push(i)
-			else:
-				val1=self.pop()
-				val2=self.pop()
-				self.push(str(eval(val2+i+val1)))
-		return self.pop()
+op = {"+", "-", "*", "/", "(", ",", ")", "^", }
+
+
+def eval_postf(exp):
+    stack = []
+    for i in exp:
+        if i not in op:
+            stack.append(i)
+        else:
+            a = int(stack.pop())
+            b = int(stack.pop())
+
+            if i == "+":
+                res = b+a
+            elif i == "-":
+                res = b-a
+            elif i == "*":
+                res = b*a
+            elif i == "%":
+                res = b % a
+            elif i == "/":
+                res = b/a
+            elif i == "**":
+                res = b**a
+
+            stack.append(res)
+    return "".join(map(str, stack))
+
+
 exp = input("Enter Postfix expression")
-obj = evalPostfix(len(exp))
-find = obj.EPF(exp)
-print(f"Result of Postfix expression {exp} is {int(float(find))}")
+print(f"Result of Postfix expression {exp} is", eval_postf(exp))
