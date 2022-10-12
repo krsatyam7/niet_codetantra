7b4dfd7f87a012cc793e57ebafe42be54f41c2f8
Kr Satyam <110342305+krsatyam7@users.noreply.github.com>
2022-10-12 23:03:19 +0530

    Update StringBuilderDemo.java

--- Object Oriented Techniques using Java Lab/LAB 08/StringBuilderDemo.java
+++ Object Oriented Techniques using Java Lab/LAB 08/StringBuilderDemo.java
@@ -1,20 +1,27 @@
 package q24216;
+
 public class StringBuilderDemo {
-	public static void main(String args[]) {
-		String s = new String("AB");
-		System.out.print("In Strings before concatenation Hash Code is: ");
-		System.out.println(s.hashCode());
-		s += "C";
-		// print hash code after concatenating
-		
-		StringBuilder sb = new StringBuilder("AB");
-		// print hash code before concatenating
-		
-		// add string C to AB 
-		
-		// print hash code after concatenating
-		
-		// and observe the output
-		
-	}
+
+  public static void main(String args[]) {
+    String s = new String("AB");
+    System.out.print("In Strings before concatenation Hash Code is: ");
+    System.out.println(s.hashCode());
+    s += "C";
+    System.out.println(
+      "In Strings after concatenation Hash Code is: " + s.hashCode()
+    );
+    StringBuilder sb = new StringBuilder("AB");
+    // print hash code before concatenating
+    System.out.println(
+      "In StringBuilder before concatenation Hash Code is: " + sb.hashCode()
+    );
+    // add string C to AB
+    sb.append("C");
+    // print hash code after concatenating
+    System.out.println(
+      "In StringBuilder after concatenation Hash Code is: " + sb.hashCode()
+    );
+    // and observe the output
+
+  }
 }
