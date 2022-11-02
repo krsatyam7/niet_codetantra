9577cebd5a973d3ec169aa5b17e5e7c47bf701bb
Kr Satyam <110342305+krsatyam7@users.noreply.github.com>
2022-11-02 15:43:46 +0530

    Delete Problem Solving using Python Lab - I Year II Sem/Unit-4 directory

--- Problem Solving using Python Lab - I Year II Sem/Unit-4/charCount.py
+++
@@ -1,9 +0,0 @@
-str1=input("Please enter sentence: ")
-mylist=[]
-for i in str1:
-	if i not in mylist:
-		mylist.append(i)
-mylist.sort()
-for j in mylist:
-	print("'"+str(j)+"'",str1.count(j),sep="\t")
-print(mylist)
