699e66696603fa58dd775a8d475a8260b00e831f
Kumar Satyam <110342305+krsatyam7@users.noreply.github.com>
2023-01-20 18:20:09 +0530

    Create divideAndConquer.c

---
+++ Design and Analysis of Algorithm Lab/2. Divide & Conquer/divideAndConquer.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+  int arr[10], n, i, max, min;
+  printf ("Enter the total number of Elements : ");
+  scanf ("%d", &n);
+  printf ("Enter the numbers : ");
+  for (i = 0; i < n; i++)
+    {
+      scanf ("%d", &arr[i]);
+    }
+  max = min = arr[0];
+  for (i = 0; i < n; i++)
+    {
+      if (arr[i] > max)
+	{
+	  max = arr[i];
+	}
+      if (arr[i] < min)
+	{
+	  min = arr[i];
+	}
+    }
+  printf ("Minimum element in an array : %d", min);
+  printf ("\nMaximum element in an array : %d\n", max);
+}
