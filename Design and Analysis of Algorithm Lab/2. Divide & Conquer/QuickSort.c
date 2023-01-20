68792801fc178ae6b05151a984b7a85791a0ea8e
Kumar Satyam <110342305+krsatyam7@users.noreply.github.com>
2023-01-20 18:17:58 +0530

    Create QuickSort.c

---
+++ Design and Analysis of Algorithm Lab/2. Divide & Conquer/QuickSort.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<conio.h>
+void quicksort (int num[100], int first, int last)
+{
+  int i, j, pivot, temp;
+  if (first < last)
+    {
+      pivot = first;
+      i = first;
+      j = last;
+      while (i < j)
+	{
+	  while (num[i] <= num[pivot] && i < last)
+	    i++;
+	  while (num[j] > num[pivot])
+	    j--;
+	  if (i < j)
+	    {
+	      temp = num[i];
+	      num[i] = num[j];
+	      num[j] = temp;
+	    }
+	}
+      temp = num[pivot];
+      num[pivot] = num[j];
+      num[j] = temp;
+      quicksort (num, first, j - 1);
+      quicksort (num, j + 1, last);
+    }
+}
+
+void main ()
+{
+  int n, i, o[100];
+  printf ("Enter Number of elements : ");
+  scanf ("%d", &n);
+  printf ("Enter %d Elements : ", n);
+  for (i = 0; i < n; i++)
+    {
+      scanf ("%d", &o[i]);
+    }
+  quicksort (o, 0, n - 1);
+  printf ("Sorted Numbers are : ");
+  for (i = 0; i < n; i++)
+    {
+      printf ("%d ", o[i]);
+    }
+  {
+    printf ("\n");
+  }
+}
