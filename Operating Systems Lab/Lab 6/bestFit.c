a94e2c1f036f20dcbbd659c6de72dc9bd1fbf81e
Kumar Satyam <110342305+krsatyam7@users.noreply.github.com>
2023-01-28 22:40:55 +0530

    Create bestFit.c

---
+++ Operating Systems Lab/Lab 6/bestFit.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<conio.h>
+#define max 25
+int main(){
+	int frag[max],b[max],f[max],i,j,nb,nf,temp,lowest=10000;
+	static int bf[max],ff[max];
+	printf("Memory Management Scheme for contigus memeory allocation - Best Fit\n");
+	printf("Enter the number of blocks:");
+	scanf("%d",&nb);
+	printf("Enter the number of files:");
+	scanf("%d",&nf);
+	printf("Enter the size of the blocks:-\n");
+	for(i=1;i<=nb;i++)
+	{
+		printf("Block %d:",i);
+		scanf("%d",&b[i]);
+	}
+	printf("Enter the size of the files :-\n");
+	for(i=1;i<=nf;i++)
+	{
+		printf("File %d:",i);
+		scanf("%d",&f[i]);
+	}
+	for(i=1;i<=nf;i++)
+	{
+		for(j=1;j<=nb;j++)
+		{
+			if(bf[j]!=1)
+			{
+				temp=b[j]-f[i];
+				if(temp>=0)
+				if(lowest>temp)
+				{
+					ff[i]=j;
+					lowest=temp;
+				}
+			}
+		}
+		frag[i]=lowest;
+		bf[ff[i]]=1;
+		lowest=10000;
+	}
+	printf("File No\tFile Size \tBlock No\tBlock Size\tFragment");
+	for(i=1;i<=nf && ff[i]!=0;i++)
+	printf("%d\t\t%d\t\t%d\t\t%d\t\t%d",i,f[i],ff[i],b[ff[i]],frag[i]);
+	return 0;
+}
+	
