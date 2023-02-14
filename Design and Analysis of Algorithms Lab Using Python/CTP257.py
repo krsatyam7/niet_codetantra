560fa3d97804cd932e688188792c1aa643ebe913
Kumar Satyam <110342305+krsatyam7@users.noreply.github.com>
2023-02-14 18:27:15 +0530

    Create CTP257.py

---
+++ Design and Analysis of Algorithms Lab Using Python/CTP257.py
@@ -0,0 +1,29 @@
+# Q2
+
+def binary_search(arr, x, low, high):
+	if low > high:
+		return False
+	mid = (low + high) // 2
+	if arr[mid] == x:
+		return True
+	elif arr[mid] < x:
+		return binary_search(arr, x, mid + 1, high)
+	else:
+		return binary_search(arr, x, low, mid - 1)
+		
+def main():
+	n = int(input())
+	arr = []
+	for i in range(n):
+		arr.append(int(input()))
+	x = int(input())
+	if binary_search(arr, x, 0, len(arr) - 1):
+		print("item found")
+	else:
+		print("item not found")
+if __name__ == "__main__":
+	main()
+  
+  
+
+ # failing 1 test-case (aleady tried few ways). 
