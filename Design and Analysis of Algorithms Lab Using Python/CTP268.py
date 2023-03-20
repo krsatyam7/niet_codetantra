cf2d0b05cccbf1fcb9d8091afa1f337fe3d6a0a7
Kumar Satyam <110342305+krsatyam7@users.noreply.github.com>
2023-03-20 09:57:33 +0530

    Update CTP268.py

--- Design and Analysis of Algorithms Lab Using Python/CTP268.py
+++ Design and Analysis of Algorithms Lab Using Python/CTP268.py
@@ -1,33 +1,25 @@
-# Q13
+# Q13 (answer updated)
 
-from collections import defaultdict
-
-def bfs(graph, start):
-	visited = set()
-	queue = []
-	visited.add(start)
-	queue.append(start)
-	
-	
-	while queue:
-		vertex = queue.pop(0)
-		print(vertex, end=' ')
-		for neighbor in graph[vertex]:
-			if neighbor not in visited:
-				visited.add(neighbor)
-				queue.append(neighbor)
-				
 n = int(input())
 m = int(input())
-graph = defaultdict(list)
 
-for i in range(m):
+graph = [[] for _ in range(n)]
+for _ in range(m):
 	u, v = map(int, input().split())
 	graph[u].append(v)
-	graph[v].append(u)
-
+	        
 start_vertex = int(input())
+	        
+def bfs(graph, start_vertex):
+	visited = [False] * len(graph)
+	queue = [start_vertex]
+	        	            
+	while queue:
+		vertex = queue.pop(0)
+		if not visited[vertex]:
+			print(vertex, end=' ')
+			visited[vertex] = True
+			for neighbor in graph[vertex]:
+				if not visited[neighbor]:
+					queue.append(neighbor)
 bfs(graph, start_vertex)
-
-
-# failing 1 hidden testcase :(
