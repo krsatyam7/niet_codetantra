0f4c2f58a6d62377002dbf16f3082e99941c15bf
Kumar Satyam <110342305+krsatyam7@users.noreply.github.com>
2023-03-20 09:48:14 +0530

    Update CTP267.py

--- Design and Analysis of Algorithms Lab Using Python/CTP267.py
+++ Design and Analysis of Algorithms Lab Using Python/CTP267.py
@@ -1,32 +1,28 @@
-# Q12
+# Q12 (answer updated)
 
-def dfs(graph, start, visited):
-	visited.add(start)
-	print(start, end=" ")
-	
-	for neighbor in graph[start]:
-		if neighbor not in visited:
-			dfs(graph, neighbor, visited)
-			
 n = int(input())
 m = int(input())
 
-# Creating an adjacency list for the graph
-graph = {i: [] for i in range(n)}
-
-# Populating the adjacency list
-for i in range(m):
+graph = [[] for _ in range(n)]
+for _ in range(m):
 	u, v = map(int, input().split())
 	graph[u].append(v)
-	graph[v].append(u)
 
-# Starting vertex for DFS
-start = int(input())
-
-# Visited set to keep track of visited vertices
-visited = set()
-
-# Perform DFS
-dfs(graph,start,visited)
-
-# failing 1 hidden testcase :(
+for i in range(n):
+	graph[i].sort(reverse=True)
+	        
+start_vertex = int(input())
+	        
+def dfs(graph, start_vertex):
+	visited = [False] * len(graph)
+	stack = [start_vertex]
+	
+	while stack:
+		vertex = stack.pop()
+		if not visited[vertex]:
+			print(vertex, end=' ')
+			visited[vertex] = True
+			for neighbor in graph[vertex]:
+				if not visited[neighbor]:
+					stack.append(neighbor)
+dfs(graph, start_vertex)
