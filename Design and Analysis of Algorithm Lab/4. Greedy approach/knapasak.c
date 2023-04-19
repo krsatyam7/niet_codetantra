8b749a971543afdc8a1078083194d6d369e4df20
Kumar Satyam <110342305+krsatyam7@users.noreply.github.com>
2023-04-19 15:39:34 +0530

    Update knapasak.c

--- Design and Analysis of Algorithm Lab/4. Greedy approach/knapasak.c
+++ Design and Analysis of Algorithm Lab/4. Greedy approach/knapasak.c
@@ -41,7 +41,9 @@ int main(){
 	  }
 	  for (i = 1; i <= num; i++){
 	  	for (j = i + 1; j <= num; j++){
-			// remove } from below line
+			
+			// remove any  '}' from below code block, if produced
+			
 	  		if (ratio[i] < ratio[j]){
 	  			temp = ratio[j];
 	  			ratio[j] = ratio[i];
