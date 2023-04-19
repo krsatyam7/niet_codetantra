52cefcd2371b8c04e735ee4959dae64a34651b8a
Kumar Satyam <110342305+krsatyam7@users.noreply.github.com>
2023-04-19 15:45:33 +0530

    Update dijkstrasAlgorithm.c

--- Design and Analysis of Algorithm Lab/4. Greedy approach/dijkstrasAlgorithm.c
+++ Design and Analysis of Algorithm Lab/4. Greedy approach/dijkstrasAlgorithm.c
@@ -5,16 +5,16 @@ int n, k;
 #define perm 1
 #define tent 2
 #define infinity INT_MAX
-typedef struct nodelabel {
+typedef struct nodelabel{
   int predecessor;
   int length;
   int label;
   int number;
 }
 nodelabel;
-void initialize_single_source(nodelabel state[], int s, int n) {
+void initialize_single_source(nodelabel state[], int s, int n){
   int i;
-  for (i = 1; i <= n; i++) {
+  for (i = 1; i <= n; i++){
     state[i].predecessor = 0;
     state[i].length = infinity;
     state[i].label = tent;
@@ -25,16 +25,16 @@ void initialize_single_source(nodelabel state[], int s, int n) {
   state[s].label = perm;
   state[s].number = s;
 }
-int parent(int i) {
+int parent(int i){
   return i / 2;
 }
-int left(int i) {
+int left(int i){
   return 2 * i;
 }
-int right(int i) {
+int right(int i){
   return 2 * i + 1;
 }
-void min_heapify(nodelabel q[], int i) {
+void min_heapify(nodelabel q[], int i){
   struct nodelabel temp;
   int l, r, smallest;
   l = left(i);
@@ -42,18 +42,18 @@ void min_heapify(nodelabel q[], int i) {
   if (l <= k && q[l].length < q[i].length) smallest = l;
   else smallest = i;
   if (r <= k && q[r].length < q[i].length) smallest = r;
-  if (smallest != i) {
+  if (smallest != i){
     temp = q[i];
     q[i] = q[smallest];
     q[smallest] = temp;
     min_heapify(q, smallest);
   }
 }
-void build_min_heap(nodelabel q[], int n) {
+void build_min_heap(nodelabel q[], int n){
   int i;
   for (i = n / 2; i >= 1; i--) min_heapify(q, i);
 }
-nodelabel heap_extract_min(nodelabel state[]) {
+nodelabel heap_extract_min(nodelabel state[]){
   nodelabel min, temp;
   min = state[1];
   temp = state[1];
@@ -63,51 +63,51 @@ nodelabel heap_extract_min(nodelabel state[]) {
   min_heapify(state, 1);
   return min;
 }
-void heap_decrease_key(nodelabel state[], int key, int i) {
+void heap_decrease_key(nodelabel state[], int key, int i){
   nodelabel temp;
   state[i].length = key;
-  while (i > 1 && state[parent(i)].length > state[i].length) {
+  while (i > 1 && state[parent(i)].length > state[i].length){
     temp = state[i];
     state[i] = state[parent(i)];
     state[parent(i)] = temp;
     i = parent(i);
   }
 }
-void relax(nodelabel u, int a[10][10], nodelabel state[], int i) {
+void relax(nodelabel u, int a[10][10], nodelabel state[], int i){
   int key;
-  if (state[i].length > (u.length + a[u.number][state[i].number])) {
+  if (state[i].length > (u.length + a[u.number][state[i].number])){
     state[i].predecessor = u.number;
     key = u.length + a[u.number][state[i].number];
     heap_decrease_key(state, key, i);
   }
 }
-void Dijkstra(int a[][10], int n, int s) {
+void Dijkstra(int a[][10], int n, int s){
   nodelabel state[10], min;
   int i, count, j, x, dist = 0;
   int path[10];
   initialize_single_source(state, s, n);
   build_min_heap(state, n);
-  while (k != 0) {
+  while (k != 0){
     min = heap_extract_min(state);
     for (i = 1; i <= k; i++)
       if (a[min.number][state[i].number] > 0 && state[i].label == tent) relax(min, a, state, i);
     min.label = perm;
   }
   for (i = 1; i <= n; i++)
-    if (i != s) {
+    if (i != s){
       j = i;
       dist = 0;
       count = 0;
-      do {
+      do{
         count++;
         path[count] = j;
         for (k = 1; k <= n; k++)
-          if (state[k].number == j) {
+          if (state[k].number == j){
             j = state[k].predecessor;
             break;
           }
-      } while (j != 0);
-      for (j = 1; j <= count / 2; j++) {
+      }while (j != 0);
+      for (j = 1; j <= count / 2; j++){
         x = path[j];
         path[j] = path[count - j + 1];
         path[count - j + 1] = x;
@@ -121,7 +121,7 @@ void Dijkstra(int a[][10], int n, int s) {
       printf("\n");
     }
 }
-int main() {
+int main(){
   int a[10][10], i, j, source;
   printf("Enter the number of nodes :");
   scanf("%d", & n);
