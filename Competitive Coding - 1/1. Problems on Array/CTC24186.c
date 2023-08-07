f8925c1943cc8b05656b7273b8cb8a8996e2eadc
Kumar Satyam <110342305+krsatyam7@users.noreply.github.com>
2023-08-07 21:16:53 +0530

    Create CTC24186.c

---
+++ Competitive Coding - 1/1. Problems on Array/CTC24186.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int sumOfArray(int arr[], int arrLen)
+{
+    int sum = 0;
+    for (int i = 0; i < arrLen; i++)
+    {
+        sum = arr[i] + sum;
+    }
+    return sum;
+}
+int readIntArray(char *argsArray, int arr[])
+{
+    int col = 0;
+    char *token = strtok(argsArray, ",");
+    while (token != NULL)
+    {
+        arr[col] = atoi(token);
+        token = strtok(NULL, ",");
+        col++;
+    }
+    return col;
+}
+int main(int argc, char *argv[])
+{
+    int arr[strlen(argv[1])];
+    int arrLen = readIntArray(argv[1], arr);
+    printf("%d\n", sumOfArray(arr, arrLen));
+    return 0;
+}
