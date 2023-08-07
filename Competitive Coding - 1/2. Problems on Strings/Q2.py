4acb97507c56ac7ab2f396063d325451199b07d1
Kumar Satyam <110342305+krsatyam7@users.noreply.github.com>
2023-08-07 22:23:03 +0530

    Create Q2.py

---
+++ Competitive Coding - 1/2. Problems on Strings/Q2.py
@@ -0,0 +1,25 @@
+import sys
+
+s = sys.argv[1]
+
+def isPrimeLengthPalindrome(s):
+
+	n=len(s)
+	rev=""
+	flag=0
+	if n>1:
+		for i in range(2,n):
+			if n%i==0:
+				flag=1
+				break
+	if flag==1:
+		return False
+	else:
+		for i in s:
+			rev=i+rev
+		if rev==s and flag==0:
+			return True
+		else:
+			return False
+
+print("true" if isPrimeLengthPalindrome(s) else "false");
