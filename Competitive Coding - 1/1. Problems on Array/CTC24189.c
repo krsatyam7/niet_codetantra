9081a1abee9ea0903f9b77cf4b8ba7cb122456b8
Kumar Satyam <110342305+krsatyam7@users.noreply.github.com>
2023-08-07 21:21:07 +0530

    Create CTC24189.c

---
+++ Competitive Coding - 1/1. Problems on Array/CTC24189.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int maxSumContiguousSubArray(int arr[], int arrLen)
+{
+    int max = 0;
+    int maxend = 0;
+    for (int i = 0; i < arrLen; i++)
+    {
+        maxend = maxend + arr[i];
+        if (maxend < 0)
+        {
+            maxend = 0;
+        }
+        if (max < maxend)
+        {
+            max = maxend;
+        }
+    }
+    return max;
+}
+int readIntArray(char *argsArray, int arr[])
+{
+    int col = 0;
+    char *token = strtok(argsArray, ",");
+    while (token != NULL)
+    {
+        arr[col] = atoi(token);
+        token = strtok(NULL, ",");
+        col++;
+    }
+    return col;
+}
+int main(int argc, char *argv[])
+{
+    int arr[strlen(argv[1])];
+    int arrLen = readIntArray(argv[1], arr);
+    printf("%d\n", maxSumContiguousSubArray(arr, arrLen));
+    return 0;
+}
