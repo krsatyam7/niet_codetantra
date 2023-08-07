1dc08fd54c2813c3c06bae656b502c233db72b89
Kumar Satyam <110342305+krsatyam7@users.noreply.github.com>
2023-08-07 22:24:49 +0530

    Create Q5.py

---
+++ Competitive Coding - 1/2. Problems on Strings/Q5.py
@@ -0,0 +1,17 @@
+import sys
+
+S = sys.argv[1]
+
+def consecutiveString(S):
+	ans = ''
+	stack = S[0]
+	for i in range(len(S) -1):
+		if S[i+1]!=stack[-1]:
+			ans += stack
+			stack = S[i+1]
+		else:
+			stack += S[i+1]
+			if len(stack) > 2:
+				stack = stack[:2]
+	return ans+stack
+print(consecutiveString(S))
