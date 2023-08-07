6b09f49855fe52968254529ee3f5520cd2b264aa
Kumar Satyam <110342305+krsatyam7@users.noreply.github.com>
2023-08-07 21:19:37 +0530

    Create 24188.c

---
+++ Competitive Coding - 1/1. Problems on Array/24188.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int middleElementOfArray(int arr[], int arrLen)
+{
+    return arrLen % 2 == 0 ? arr[arrLen / 2 - 1] : arr[arrLen / 2];
+}
+
+int readIntArray(char *argsArray, int arr[])
+{
+    int col = 0;
+    char *token = strtok(argsArray, ",");
+    while (token != NULL)
+    {
+        arr[col] = atoi(token);
+        token = strtok(NULL, ",");
+        col++;
+    }
+    return col;
+}
+
+int main(int argc, char *argv[])
+{
+    int arr[strlen(argv[1])];
+    int arrLen = readIntArray(argv[1], arr);
+    printf("%d\n", middleElementOfArray(arr, arrLen));
+    return 0;
+}
