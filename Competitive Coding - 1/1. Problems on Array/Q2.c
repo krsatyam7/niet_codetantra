96777667c56de02f9bb83f6737a6b0641e1d0007
Kumar Satyam <110342305+krsatyam7@users.noreply.github.com>
2023-08-10 21:20:17 -0700

    Update Q2.c

--- Competitive Coding - 1/1. Problems on Array/Q2.c
+++ Competitive Coding - 1/1. Problems on Array/Q2.c
@@ -1,32 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-int sumOfHighestAndLowestElements(int arr[], int arrLen)
-{
-    int min = arr[0], max = arr[0];
-    for (int i = 1; i < arrLen; i++)
-    {
-        if (arr[i] > max)
-            max = arr[i];
-        if (arr[i] < min)
-            min = arr[i];
+int sumOfHighestAndLowestElements(int arr[], int arrLen) {
+    // Write from code here
+    int min=arr[0],max=arr[0];
+    for(int i=1;i<arrLen;i++){
+        if(arr[i]>max)
+            max=arr[i];
+        if(arr[i]<min)
+            min=arr[i];
     }
-    return max + min;
+        return max+min;
+    // ends here
 }
-int readIntArray(char *argsArray, int arr[])
-{
+int readIntArray(char *argsArray, int arr[]) {
     int col = 0;
     char *token = strtok(argsArray, ",");
-    while (token != NULL)
-    {
+    while (token != NULL) {
         arr[col] = atoi(token);
         token = strtok(NULL, ",");
         col++;
     }
     return col;
 }
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int arr[strlen(argv[1])];
     int arrLen = readIntArray(argv[1], arr);
     printf("%d\n", sumOfHighestAndLowestElements(arr, arrLen));
