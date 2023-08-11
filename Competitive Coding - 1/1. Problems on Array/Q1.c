6c639cc6f350953228e3c5ddcb02a286cc1ecb80
Kumar Satyam <110342305+krsatyam7@users.noreply.github.com>
2023-08-10 21:18:01 -0700

    Update Q1.c

--- Competitive Coding - 1/1. Problems on Array/Q1.c
+++ Competitive Coding - 1/1. Problems on Array/Q1.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int sumOfArray(int arr[], int arrLen)
-{
-    int sum = 0;
-    for (int i = 0; i < arrLen; i++)
-    {
-        sum = arr[i] + sum;
+int sumOfArray(int arr[], int arrLen) {
+    // write code from here 
+    int sum=0;
+    for(int i=0;i<arrLen;i++){
+        sum=arr[i]+sum;
     }
     return sum;
+    // ends here
 }
-int readIntArray(char *argsArray, int arr[])
-{
+int readIntArray(char *argsArray, int arr[]) {
     int col = 0;
     char *token = strtok(argsArray, ",");
-    while (token != NULL)
-    {
+    while (token != NULL) {
         arr[col] = atoi(token);
         token = strtok(NULL, ",");
         col++;
     }
     return col;
 }
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int arr[strlen(argv[1])];
     int arrLen = readIntArray(argv[1], arr);
     printf("%d\n", sumOfArray(arr, arrLen));
