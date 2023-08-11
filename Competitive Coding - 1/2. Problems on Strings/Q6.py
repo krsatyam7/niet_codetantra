f16e87f820d370d337791705caad3449b6e92839
Kumar Satyam <110342305+krsatyam7@users.noreply.github.com>
2023-08-11 10:30:33 +0530

    Update Q6.py

--- Competitive Coding - 1/2. Problems on Strings/Q6.py
+++ Competitive Coding - 1/2. Problems on Strings/Q6.py
@@ -1,9 +1,12 @@
 import sys
 
 S = sys.argv[1]
+
+# write below line first
 arr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
 def reverseAlphas(S):
+	# write code from here
 	arr1 = []
 	for i in S:
 		if i in arr:
@@ -15,5 +18,6 @@ def reverseAlphas(S):
 		else:
 			ans += i
 	return ans
+	# ends here
 
 print(reverseAlphas(S))
