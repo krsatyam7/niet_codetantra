bf28a7c33905edf08cffa881ca100a0dc7f32701
Kumar Satyam <110342305+krsatyam7@users.noreply.github.com>
2023-08-11 10:43:22 +0530

    Create Q6.py

---
+++ Competitive Coding - 1/3. Problems on Mathematics/Q6/Q6.py
@@ -0,0 +1,18 @@
+import sys
+
+d1 = int(sys.argv[1])
+d2 = int(sys.argv[2])
+c = int(sys.argv[3])
+L = int(sys.argv[4])
+
+def isDivisibleBy41(d1, d2, c, L):
+	digits = [d1,d2]+[0]*(L-2)
+	for i in range(2, L):
+		digits[i] = (digits[i-1]*c + digits[i-2])%10
+	num = 0
+	for i in range(L):
+		num  += digits[i]*10**(L-i-1)
+	return num % 41 == 0
+
+
+print("true" if isDivisibleBy41(d1, d2, c, L) else "false");
