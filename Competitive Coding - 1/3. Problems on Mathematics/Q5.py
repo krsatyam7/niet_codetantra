1e8aca337f949ff95c7a1fbbaf26be2cb0359cc8
Kumar Satyam <110342305+krsatyam7@users.noreply.github.com>
2023-08-11 10:54:34 +0530

    Update Q5.py

--- Competitive Coding - 1/3. Problems on Mathematics/Q5.py
+++ Competitive Coding - 1/3. Problems on Mathematics/Q5.py
@@ -5,6 +5,8 @@ L = int(sys.argv[2])
 R = int(sys.argv[3])
 
 def sumBetween(N, L, R):
+	# write code from here 
 	arr = [i for i in range(0, N+1, 2)] + [i for i in range(1, N+1, 2)]
 	return sum(arr[L:R+1])
+	# ends here
 print(sumBetween(N, L, R))
