d52cb28410e382ed06d5c9f2b2fcdba9943afa94
Kumar Satyam <110342305+krsatyam7@users.noreply.github.com>
2023-08-11 10:49:05 +0530

    Update Q2.py

--- Competitive Coding - 1/3. Problems on Mathematics/Q2.py
+++ Competitive Coding - 1/3. Problems on Mathematics/Q2.py
@@ -5,9 +5,11 @@ r2 = float(sys.argv[2])
 r3 = float(sys.argv[3])
 
 def combinedResistanceParallel(r1, r2, r3):
+	# write from here
 	try:
 		return round((r1*r2*r3) / (r1*r2 + r2*r3 + r3*r1), 6)
 	except:
 		return -1
+	# ends here
 
 print("%1.6e" % combinedResistanceParallel(r1, r2, r3))
