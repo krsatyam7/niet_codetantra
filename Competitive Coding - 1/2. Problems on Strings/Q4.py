da782790b79b1ba97c642ff09fd5ddff9a912d40
Kumar Satyam <110342305+krsatyam7@users.noreply.github.com>
2023-08-11 10:26:04 +0530

    Update Q4.py

--- Competitive Coding - 1/2. Problems on Strings/Q4.py
+++ Competitive Coding - 1/2. Problems on Strings/Q4.py
@@ -1,17 +1,20 @@
 import sys
 
 s = sys.argv[1]
+# write 1st part of code from here
 keys=[chr(x) for x in range(97,123)]
 values=[x for x in range(1,27)]
 myDict={k:v for (k,v) in zip(keys, values)}
+# ends here
 
 def isSuper(s):
-	# Write code here
+	# write 2nd part of code from here
 	Is=[x for x in s]
 	se=set(Is)
 	for i in s:
 		if s.count(i) != myDict[i]:
 			return False
 	return True
+	# ends here
 
 print("true" if isSuper(s) else "false");
