46b8a93bd1b541459762fb6bdd8bdf3b8f1c960c
Kumar Satyam <110342305+krsatyam7@users.noreply.github.com>
2023-08-10 21:42:29 -0700

    Update Q6.py

--- Competitive Coding - 1/1. Problems on Array/Q6.py
+++ Competitive Coding - 1/1. Problems on Array/Q6.py
@@ -1,10 +1,12 @@
 import sys
 arr = sys.argv[1].split(',')
-
 def lengthIndexMatch(arr):
-	ans=""
-	for i in range(len(arr)):
-		if len(arr[i])==i:
-			ans+=arr[i]
-	return ans if len(ans)!=0 else "no match found"
+    # write code from here
+    ans=""
+    for i in range(len(arr)):
+        if len(arr[i])==i:
+            ans+=arr[i]
+    return ans if len(ans)!=0 else "no match found"
+    # ends here
+    
 print(lengthIndexMatch(arr))
