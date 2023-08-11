38c40ada4fa80dc1a186fd7d89941d91a282b339
Kumar Satyam <110342305+krsatyam7@users.noreply.github.com>
2023-08-11 12:14:46 +0530

    Update Q1.py

--- Competitive Coding - 1/2. Problems on Strings/Q1.py
+++ Competitive Coding - 1/2. Problems on Strings/Q1.py
@@ -5,14 +5,9 @@ l = int(sys.argv[2])
 
 def getFirstSubString(s, l):
 	# write code from here
-	for i in range(len(s)):
-		substring=s[:i+1:2]
-		if len(substring)==l:
-			break
-	if len(substring)>0 and len(substring)==l:
-		return substring
-	else:
-		return "NotFound"
+	if len(s) < l*2-1:
+		return "Not Found"
+	return s[:l*2:2]
 	#ends here
 
 print(getFirstSubString(s, l))
