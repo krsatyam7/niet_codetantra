0881dfd87472cbddf9b20d1dc7faea51c7ed0b55
Kumar Satyam <110342305+krsatyam7@users.noreply.github.com>
2023-08-10 21:22:12 -0700

    Update Q4.c

--- Competitive Coding - 1/1. Problems on Array/Q4.c
+++ Competitive Coding - 1/1. Problems on Array/Q4.c
@@ -1,38 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-int maxSumContiguousSubArray(int arr[], int arrLen)
-{
-    int max = 0;
-    int maxend = 0;
-    for (int i = 0; i < arrLen; i++)
-    {
-        maxend = maxend + arr[i];
-        if (maxend < 0)
-        {
-            maxend = 0;
+    int max=0;
+    int maxend=0;
+    for(int i=0;i<arrLen;i++){
+        maxend=maxend+arr[i];
+        if(maxend<0){
+            maxend=0;
         }
-        if (max < maxend)
-        {
-            max = maxend;
+        if(max<maxend){
+            max=maxend;
         }
     }
     return max;
+    //ends here
 }
-int readIntArray(char *argsArray, int arr[])
-{
+int readIntArray(char *argsArray, int arr[]) {
     int col = 0;
     char *token = strtok(argsArray, ",");
-    while (token != NULL)
-    {
+    while (token != NULL) {
         arr[col] = atoi(token);
         token = strtok(NULL, ",");
         col++;
     }
     return col;
 }
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int arr[strlen(argv[1])];
     int arrLen = readIntArray(argv[1], arr);
     printf("%d\n", maxSumContiguousSubArray(arr, arrLen));
