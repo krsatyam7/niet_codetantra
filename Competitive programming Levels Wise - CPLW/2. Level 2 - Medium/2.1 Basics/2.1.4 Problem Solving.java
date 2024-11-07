1bf98796c34204602ebfdf0c239b8b97983dbd90
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2024-11-08 01:17:47 +0530

    Create 2.1.4 Problem Solving.java

---
+++ Competitive programming Levels Wise - CPLW/2. Level 2 - Medium/2.1 Basics/2.1.4 Problem Solving.java
@@ -0,0 +1,17 @@
+package q23512;
+public class CTJ23512 {
+        public static int productOfFactors(int n) {
+                int product = 1;
+                for (int i = 1; i <= n; i++) {
+                    if (n % i == 0) {
+                        product *= i;
+                    }
+                }
+                return product;
+        }
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        System.out.println(productOfFactors(n));
+    }
+}
