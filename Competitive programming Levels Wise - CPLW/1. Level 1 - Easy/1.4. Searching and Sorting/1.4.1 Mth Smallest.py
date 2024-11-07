c3f791745122cfa5a892158dbb17dabdaa65a204
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2024-11-08 01:29:12 +0530

    Create 1.4.1 Mth Smallest.py

---
+++ Competitive programming Levels Wise - CPLW/1. Level 1 - Easy/1.4. Searching and Sorting/1.4.1 Mth Smallest.py
@@ -0,0 +1,7 @@
+import sys
+Arr = [int(x) for x in sys.argv[1].split(',')]
+M = int(sys.argv[2])
+def MthSmallestElement(Arr, M):
+    Arr.sort()
+    return Arr[M-1]
+print(MthSmallestElement(Arr, M))
