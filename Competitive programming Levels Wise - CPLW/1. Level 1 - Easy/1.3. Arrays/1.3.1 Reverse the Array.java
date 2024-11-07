2accb51323dfb698cadc6f3567bc35b06fe16018
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2024-11-08 01:26:38 +0530

    Create 1.3.1 Reverse the Array.java

---
+++ Competitive programming Levels Wise - CPLW/1. Level 1 - Easy/1.3. Arrays/1.3.1 Reverse the Array.java
@@ -0,0 +1,22 @@
+package q1555;
+import java.util.*;
+import java.util.stream.*;
+public class CTJ1555 {
+    public static List<Integer> reverseArray(List<Integer> Arr) {
+        int len = Arr.size();
+        int r = len-1;
+        int l = 0;
+        while(l<=r){
+            int temp = Arr.get(l);
+            Arr.set(l,Arr.get(r));
+            Arr.set(r,temp);
+            l++;
+            r--;
+        }
+        return Arr;
+    }
+    public static void main(String[] args) {
+        List<Integer> Arr = Arrays.stream(args[0].split(",")).map(s -> Integer.parseInt(s)).collect(Collectors.toList());
+        System.out.println(reverseArray(Arr).stream().map(String::valueOf).collect(Collectors.joining(",")));
+    }
+}
