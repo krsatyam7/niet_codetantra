aacb9ad332a12f4b2c59d6acd1bdd72becf7a7ab
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2024-11-08 14:45:16 +0530

    1.1.1 Armstrong (Java)

--- Competitive programming Levels Wise - CPLW/1. Level 1 - Easy/1.1. Basics/1.1.1. Armstrong.java
+++ Competitive programming Levels Wise - CPLW/1. Level 1 - Easy/1.1. Basics/1.1.1. Armstrong.java
@@ -13,7 +13,6 @@ public class CTJ3341 {
             return "no";
         }
     }
-
     public static void main(String[] args) {
         int num = Integer.parseInt(args[0]);
         System.out.println(checkArmstrong(num));
